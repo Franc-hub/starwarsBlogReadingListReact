d9141da43f05ead0c514910be5bea37f48016725
Franc Cosp <franccospmeyaeso@gmail.com>
2021-06-08 16:35:07 +0000

    login added

--- src/js/views/Login.js
+++ src/js/views/Login.js
@@ -2,45 +2,61 @@ import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/login.scss";
 const Login = () => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { store, actions } = useContext(Context);
+    const body = {
+        email: email,
+        password: password
+    }
 
-	function validateFormFront() {
-		return email.length > 0 && password.length > 0;
-	}
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }
+    function validateFormFront() {
+        return email.length > 0 && password.length > 0;
+    }
 
-	function handleSubmit(event) {
-		event.preventDefault();
-	}
-	return (
-		<>
-			<div className="jumbotron jumbotron-fluid">
-				<div className="container">
-					<h1 className="display-4">Login</h1>
-					<p className="lead">
-						<div>
-							<input onChange={e => setEmail(e.target.value)} value={email} placeholder="Send email..." />
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
 
-							<input
-								onChange={e => setPassword(e.target.value)}
-								value={password}
-								placeholder="Send password..."
-							/>
-						</div>
+    const SubmitData = () => {
+        fetch()
+    }
+    return (
+        <>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Login</h1>
+                    <p className="lead">
+                        <div>
+                            <input onChange={e => setEmail(e.target.value)} value={email} placeholder="Send email..." />
 
-						<button
-							onClick={() => {
-								validateFormFront();
-								handleSubmit();
-							}}>
-							Send
+                            <input
+                                onChange={e => setPassword(e.target.value)}
+                                value={password}
+                                placeholder="Send password..."
+                            />
+                        </div>
+
+                        <button
+                            onClick={() => {
+                                validateFormFront();
+                                handleSubmit();
+                            }}>
+                            Send
 						</button>
-					</p>
-				</div>
-			</div>
-		</>
-	);
-	0;
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+    0;
 };
 
 export default Login;
