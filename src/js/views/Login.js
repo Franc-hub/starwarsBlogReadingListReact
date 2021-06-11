1baab4d83fffeff71bae4c27d6f1a7aa933acf89
Franc Cosp <franccospmeyaeso@gmail.com>
2021-06-11 13:50:04 +0000

    improving in login component

--- src/js/views/Login.js
+++ src/js/views/Login.js
@@ -2,61 +2,55 @@ import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/login.scss";
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const { store, actions } = useContext(Context);
-    const body = {
-        email: email,
-        password: password
-    }
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const { store, actions } = useContext(Context);
+	const body = {
+		email: email,
+		password: password
+	};
 
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(body)
-    }
-    function validateFormFront() {
-        return email.length > 0 && password.length > 0;
-    }
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(body)
+	};
 
-    function handleSubmit(event) {
-        event.preventDefault();
-    }
+	const SubmitData = () => {
+		fetch("https://3000-tomato-lynx-ge3mdj1p.ws-eu09.gitpod.io/login", options)
+			.then(res => res.json())
+			.then(json => console.log(json));
+	};
+	return (
+		<>
+			<div className="jumbotron jumbotron-fluid">
+				<div className="container">
+					<h1 className="display-4">Login</h1>
+					<p className="lead">
+						<div>
+							<input onChange={e => setEmail(e.target.value)} value={email} placeholder="Send email..." />
 
-    const SubmitData = () => {
-        fetch()
-    }
-    return (
-        <>
-            <div className="jumbotron jumbotron-fluid">
-                <div className="container">
-                    <h1 className="display-4">Login</h1>
-                    <p className="lead">
-                        <div>
-                            <input onChange={e => setEmail(e.target.value)} value={email} placeholder="Send email..." />
+							<input
+								onChange={e => setPassword(e.target.value)}
+								value={password}
+								placeholder="Send password..."
+							/>
+						</div>
 
-                            <input
-                                onChange={e => setPassword(e.target.value)}
-                                value={password}
-                                placeholder="Send password..."
-                            />
-                        </div>
-
-                        <button
-                            onClick={() => {
-                                validateFormFront();
-                                handleSubmit();
-                            }}>
-                            Send
+						<button
+							onClick={() => {
+								SubmitData();
+							}}>
+							Login
 						</button>
-                    </p>
-                </div>
-            </div>
-        </>
-    );
-    0;
+					</p>
+				</div>
+			</div>
+		</>
+	);
+	0;
 };
 
 export default Login;
